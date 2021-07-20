8384da9071aca701f27356801c77e62f783a3fe6
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2021-07-20 12:07:14 -0300

    Create operadores.js

---
+++ operadores aritimeticos/operadores.js
@@ -0,0 +1,44 @@
+//somar( + ), diminuir( - ), multiplicar( * ), dividir( / )
+//resto da divisao( % ), potencia( ** )
+
+//OPERADORES ARITIMÉTICO
+
+let n1 = 10
+let n2 = 5
+console.log(n1 + n2);
+console.log(n1 - n2);
+console.log(n1 * n2);
+console.log(n1 / n2);
+console.log(n1 % n2);
+console.log(2 ** 3 );
+
+//OPERADORES DE ATRIBUIÇÃO
+
+let n3 = 27;
+//n3 = n3 + 15; ou reduzindo
+//n3 += 15;
+console.log(n3);
+
+//INCREMENTO E DECREMENTO
+let i = 0;
+i++;
+console.log(i);
+
+//COMPARAÇÃO
+/*
+    igualdade de valor == 
+    igualdade de valor e tipo === 
+    <, >, <=, >=
+    != valores diferentes
+    !!= valores e tipos diferentes
+*/
+console.log(n1, n2, n3);
+console.log(n2 == 10);
+console.log(n2 === 10);
+console.log(n2 > 10);
+console.log(n2 < 10);
+console.log(n2 >= 10);
+console.log(n2 <= 10);
+console.log(n2 != '10');
+console.log(n2 !== '10');
+
