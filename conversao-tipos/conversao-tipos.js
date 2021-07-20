fe5bd68999795cd1b45b5438aeeefea95dbd029c
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2021-07-20 06:27:33 -0300

    Create conversao-tipos.js

---
+++ conversao-tipos/conversao-tipos.js
@@ -0,0 +1,24 @@
+parseFloat, parseInt, Number()
+
+parseInt
+//let n1 =  '10' ;
+//n1 =  parseInt(n1);
+//let n2 = 2;
+//console.log(n1 + n2, typeof n1, typeof n2);
+
+parseFloat
+//let n1 =  '10.64854848' ;
+//n1 =  parseFloat(n1);
+//let n2 = 2;
+//console.log(n1 + n2, typeof n1, typeof n2, n1);
+
+Number()
+//let n1 =  '14' ;
+//n1 =  Number(n1);
+//let n2 = 2;
+//console.log(n1 + n2, typeof n1, typeof n2, n1);
+
+//conversao de numero para string usar aspas vazias ou toString()
+n2 = 11;
+n2 = n2.toString();
+console.log(n2, typeof n2);
