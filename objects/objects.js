e125dd8c40d03b059ebf7a822bbc8d41576dcfab
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2021-07-21 17:57:49 -0300

    Create objects.js

---
+++ objects/objects.js
@@ -0,0 +1,13 @@
+//const nomes = ['eu', 'tu', 'ele']
+//const idades = [38, 25, 15]
+//console.log(nomes[0], idades[0])
+
+//        sintax formal
+
+const pessoa = new Object()
+pessoa.nome = 'Alexandre'
+pessoa.idades = 38
+console.log(pessoa)
+let prop = 'nome'
+console.log(pessoa[prop])
+console.log(pessoa['idades'])
