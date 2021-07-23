8dd62f6da6f5c90bd0c8f946e91e1a4c57f75fd7
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2021-07-23 18:26:12 -0300

    Create array_while.js

---
+++ array while/array_while.js
@@ -0,0 +1,20 @@
+//criar um array com numeros randomicos não repetidos
+
+function generateRandomInteger(max){
+    return parseInt(Math.random() * max)
+}
+
+let arr = []
+let i = 0
+while(arr.length <= 20){
+    i++
+    let randomNumber = generateRandomInteger(30)
+
+    if(arr.indexOf(randomNumber) < 0){
+        arr.push(randomNumber)
+    }else{
+        console.log(randomNumber, 'já existe no arr')
+    }
+}
+console.log(arr)
+console.log('o loop foi executado ', i, 'vezes')
