8a9258836e764759e6170170ddb6f431f2b415e3
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2021-07-23 20:27:50 -0300

    add

---
+++ Introdução/Exemplo/primeiro-exemplo.js
@@ -0,0 +1,21 @@
+
+/*
+console.log('qualquer coisa')
+comentário para várias linhas
+*/
+//comentário unica linha
+/*
+    PALAVRAS RESERVADAS
+
+    break       debugger    finally         new         typeof
+    case        default     for             return      var
+    catch       delete      function        super       void
+    class       do          if              switch      while
+    const       else        import          this        with
+    continue    export      in              throw       yield
+                extends     instanceof      try
+*/
+let nome = 'Alexandre'
+    alert(nome)
+//console.log('javascript funcionando 2222');
+
