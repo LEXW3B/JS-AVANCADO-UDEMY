e8e90f72936b797c43b5ff8d598943d0e6bec2f8
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2021-07-26 12:42:57 -0300

    Create arguments.js

---
+++ Introdução/funcoes/arguments.js
@@ -0,0 +1,12 @@
+function somar(){
+    console.log(arguments)
+    let total = 0;
+    for(let i = 0; i < arguments.length; i++){
+        total += arguments[i]
+    }
+    return total
+}
+
+console.log(somar([1, 2, 3]))
+console.log(somar([1, 2, 3, 4, 5]))
+console.log(somar([1, 2, 3, 4, 5, 6, 10, 24, 50]))
