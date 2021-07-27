e19ac6876b669d2d990ef0c53319c2e8ad3d76ea
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2021-07-26 21:24:55 -0300

    Create desafio-imc.js

---
+++ Introdução/funcoes/desafio-imc.js
@@ -0,0 +1,42 @@
+/*
+    imc = peso / altura²
+    Deve retornar um único número
+    Deve gerar um erro se receber um parâmetro não numérico
+    Deve retornar erro caso não receba nem um parâmetro
+    */
+   
+   function calcularImc(peso, altura){
+       if(peso === undefined || altura === undefined){
+           throw Error('Apenas Números')
+        }
+        return peso / (altura*altura)
+    }
+    
+    function classificaImc(imc){
+        if(imc<=16.9){
+            return 'muito abaixo do peso'
+            
+        }else if(imc<=18.4){
+            return 'abaixo do peso'
+            
+        }else if(imc<=24.9){
+            return 'normal'
+            
+        }else if(imc<=29.9){
+            return 'acima do peso' 
+            
+        }else if(imc<=34.9){
+            return 'obesidade 1'
+            
+        }else if(imc<=40){
+            return 'obesidade 2'
+            
+        }else{
+            return 'obesidade 3'
+            
+        }
+    }
+    
+    let imc = calcularImc(100, 1.69)
+    console.log(imc)
+    console.log(classificaImc())
