67d93140a8c82b1dbce0d5742c9ba97044ec7791
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2021-07-27 20:34:40 -0300

    add

---
+++ Introdução/DOM/js/saudacao.js
@@ -0,0 +1,7 @@
+(function(){
+    const nameUser = 'Alexandre'
+    //document.querySelector('.top-bar p ').textContent = 'Bem vindo(a), ' + nameUser;
+    const element = document.querySelector('.top-bar p')
+    //element.textContent = element.textContent + nameUser
+    element.textContent += nameUser
+})()
