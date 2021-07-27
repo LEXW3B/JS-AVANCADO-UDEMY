131bea0bbce735890ff84369938f5c73876cdda0
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2021-07-26 21:24:22 -0300

    Update arguments.js

--- Introdução/funcoes/arguments.js
+++ Introdução/funcoes/arguments.js
@@ -1,11 +1,12 @@
 function somar(){
     console.log(arguments)
+
     let total = 0;
     for(let i = 0; i < arguments.length; i++){
         total += arguments[i]
     }
-    return total
-}
+    return total   
+}console.log(somar.name)
 
 console.log(somar([1, 2, 3]))
 console.log(somar([1, 2, 3, 4, 5]))
