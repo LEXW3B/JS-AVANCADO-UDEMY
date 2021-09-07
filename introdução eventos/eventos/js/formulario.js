a8c45a3d23fc2a6bcd1b3a854f7e020ac1644069
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2021-09-06 22:05:47 -0300

    Update formulario.js

--- introdução eventos/eventos/js/formulario.js
+++ introdução eventos/eventos/js/formulario.js
@@ -6,13 +6,14 @@
     const btn = document.getElementById('btn')
     const formCadastro = document.querySelector('.formCadastro')
 
-    formCadastro.addEventListener('submit', function(){
-        console.log(txtTitulo.value)
-    
-        if(!txtTitulo){
-            alert('Preencha todos os campos')
+    formCadastro.addEventListener('submit', function(e){
+        console.log(txtTitulo.value)  
+        if(!txtTitulo.value){
+            showErrorMessage('Preencha todos os campos', function(){
+                txtTitulo.focus()
+            })
             e.preventDefault()//previne o comportamento padrão
-            txtTitulo.focus()
+            
         }
 
     })
@@ -22,6 +23,7 @@
     const contadorContainer = document.getElementById('contador')
     const resta = document.getElementsByTagName('span')[0]
     const maxima = txtDescricao.maxLength
+    resta.textContent = maxima
 
     //contadorContainer.removeAttribute('style') ou
     contadorContainer.style.display = 'block'
@@ -31,6 +33,38 @@
         let caractersRestantes = parseInt(maxima) - parseInt(numeroLetrasDigitadas)
         resta.textContent = caractersRestantes
     })
+
+    btn.disabled = true
+
+    const chkAceito = document.getElementById('chkAceito')
+    chkAceito.addEventListener('change', function(){
+        btn.disabled = !this.checked
+    })
+
+    const feedbackMessage = document.getElementById('feedbackMessage')
+    const feedbackMessageCloseBtn = feedbackMessage.getElementsByTagName('button')[0]
+
+    function showErrorMessage(msg, cb){
+        //alert(msg)
+        //feedbackMessage.setAttribute('class', 'show')
+        feedbackMessage.classList.add('show')
+        feedbackMessage.getElementsByTagName('p')[0].textContent = msg
+
+        function hideErrorMessage(){
+            console.log('clicando close')
+            feedbackMessage.classList.remove('show')
+
+            feedbackMessageCloseBtn.removeEventListener('click', hideErrorMessage)
+
+            if(typeof cb === 'function'){
+                cb()
+            }
+        }
+
+        feedbackMessageCloseBtn.addEventListener('click', hideErrorMessage)
+
+    }
+
 
 
 
