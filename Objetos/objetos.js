772f249b9bbf1ea61e657d11a0b7edba7a184088
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2021-09-15 18:54:08 -0300

    Create objetos.js

---
+++ Objetos/objetos.js
@@ -0,0 +1,36 @@
+//sintax literal
+const obj1 = {
+    nome: 'Alexandre'
+}
+
+// sintax formal
+const obj2 = new Object()
+obj2.nome = 'Maria'
+obj2['idade'] = 28
+
+console.log(obj1)
+console.log(obj2)
+
+//conseguindo dados primitivos com o length
+
+const str = 'minha string'
+console.log(str.length)
+
+const str2 = new String("minha string 2")
+console.log(str2)
+console.log(str2.length)
+// conseguindo dados primitivos com boolean, number, str(string), array, object
+
+
+
+
+
+
+
+
+
+
+
+
+
+
