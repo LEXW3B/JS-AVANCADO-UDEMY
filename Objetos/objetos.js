3c76413bac64c6e40d60e677c757237e50854c61
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2021-09-15 19:04:14 -0300

    Update objetos.js

--- Objetos/objetos.js
+++ Objetos/objetos.js
@@ -4,7 +4,7 @@ const obj1 = {
 }
 
 // sintax formal
-const obj2 = new Object()
+const obj2 = new Object()//construtor de objetos
 obj2.nome = 'Maria'
 obj2['idade'] = 28
 
@@ -13,16 +13,22 @@ console.log(obj2)
 
 //conseguindo dados primitivos com o length
 
-const str = 'minha string'
+const str = 'minha string'//
 console.log(str.length)
 
-const str2 = new String("minha string 2")
+const str2 = new String("minha string 2") //construtor de string
 console.log(str2)
 console.log(str2.length)
 // conseguindo dados primitivos com boolean, number, str(string), array, object
 
 
+const data1 = Date()
+console.log(data1)
+console.log(typeof data1)
 
+const data2 = new Date()
+console.log(data2)
+console.log(typeof data2)
 
 
 
