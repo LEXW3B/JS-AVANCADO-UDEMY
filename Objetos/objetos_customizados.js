34d31a0bf161a76b840971f08116ff2d24bb20f7
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2021-09-15 21:08:46 -0300

    Update objetos_customizados.js

--- Objetos/objetos_customizados.js
+++ Objetos/objetos_customizados.js
@@ -1,12 +1,12 @@
-// function changeName(name){  //poderia ser tambem com uma aerofunction
+function changeName(name){  //poderia ser tambem com uma aerofunction ?
+    this.name = name
+    this.updatedAt = new Date()
+}
+
+// const changeName = name => {//com aerofunction não iria servir porque vira windows
 //     console.log(name)
 //     console.log(this)
 // }
-
-const changeName = name => {
-    console.log(name)
-    console.log(this)
-}
 
 const task1 = {
     name: 'task1',
@@ -16,17 +16,19 @@ const task1 = {
     changeName: changeName
 }
 
-// const task2 = {
-//     name: 'task2',
-//     createAt: new Date(),
-//     updatedAt: null,
-//     completed: false
-// }
+const task2 = {
+    name: 'task2',
+    createAt: new Date(),
+    updatedAt: null,
+    completed: false,
+    changeName
+}
 task1.name = 'task 1 updated'
 task1.updatedAt = new Date()
 
-task1.changeName()
+task1.changeName('nome atualizado')
+task2.changeName('novo nome')
 
-console.log(task1)
-// console.log(task2)
+//console.log(task1)
+console.log(task2)
 
