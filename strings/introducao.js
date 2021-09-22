91cbeaca647604900d438e1cbbdc6e80ecddc227
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2021-09-22 06:29:48 -0300

    Create introducao.js

---
+++ strings/introducao.js
@@ -0,0 +1,35 @@
+/*       se tiver um * e porque não funciona em todos os browsers
+replace(),                    replaceAll(),**=-SEM SUPORTE-=**      
+indexOf(),                    lastIndexOf(),                      includes(),*
+slice(),                      substring(),                        split(),
+toLowerCase(),                toUpperCase(),          
+valueOf(),                    
+trim(),*                      trimEnd(),*                         trimStart(),*
+padStart(),*                  padEnd(),*
+startsWith(),*                endsWith(),*
+charAt(),                     charCodeAt(),                       e length*  */
+
+
+// replace(),substitui um string por outra string
+let str1 = 'minha string bacanuda'
+console.log(str1.replace('i', 'o'))
+console.log(str1)
+
+//indexOf(), vai retornar a posição de um determinada string por parametro dentro de outra string
+console.log(str1.indexOf('string'))
+
+//lastIndexOf(), vai retornar a posição da ultima ocorrencia da string que passei por parametro 
+console.log(str1.lastIndexOf('i'))
+
+
+
+
+
+
+
+
+
+
+
+
+
