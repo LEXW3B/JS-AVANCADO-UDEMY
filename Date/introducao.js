e3e97d5fee28bb9c73581733e2571641783b4525
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2021-10-14 06:24:36 -0300

    versão 1.0

---
+++ Date/introducao.js
@@ -0,0 +1,9 @@
+const data = new Date()//sem passar nada por parametro vai armazenar a data atual
+console.log(data)
+console.log(data.getDay())//dia da semana(domingo=0;sengunda=1;terça=2...)
+console.log(data.getDate())//dia do mes
+console.log(data.getMonth())//o mes(janeiro=0;fevereiro1...)
+console.log(data.getTime())//numero de milisegundo que passaram desde 1º de janeiro de 1970
+console.log(data.getFullYear())//o ano atual
+console.log(data.getHours())//a hora 
+console.log(data.getUTCHours())//hora de portugal 3 horas a mais que o brasil
