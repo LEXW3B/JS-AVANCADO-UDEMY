4d84def154770cb6d3c7e3bbec346dd16c348183
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2021-10-14 06:52:00 -0300

    versão 1.1

--- Date/introducao.js
+++ Date/introducao.js
@@ -7,3 +7,13 @@ console.log(data.getTime())//numero de milisegundo que passaram desde 1º de jan
 console.log(data.getFullYear())//o ano atual
 console.log(data.getHours())//a hora 
 console.log(data.getUTCHours())//hora de portugal 3 horas a mais que o brasil
+console.log('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
+console.log(data.toString())//transforma toda a data em string
+console.log(typeof data.toString())//confirmação da data em string 
+console.log(data.toDateString())//a data
+console.log(data.toISOString())//a data em uma nomenclatura padrão
+console.log(data.toLocaleDateString())//a data no padrão do país que a maquina foi configurada
+console.log(data.toLocaleString())//a data e hora no padrão do país que a maquina foi configurada
+console.log(data.toUTCString())//data com formato universal
+console.log(data.toLocaleString('pt-BR', {month: 'long', weekday: 'long', day: 'numeric', year: 'numeric'}))//montando um cabeçalho de data como quiser
+
