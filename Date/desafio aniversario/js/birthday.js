ff6d38dbffe9bf420b798c60c97e560ce7152bf8
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2021-10-19 20:44:48 -0300

    versão 1.2

--- Date/desafio aniversario/js/birthday.js
+++ Date/desafio aniversario/js/birthday.js
@@ -1,4 +1,25 @@
-function quantoFaltaPara(m,d){
+function quantoFaltaPara(m, d){
 
-    12121212//
+    const dataAtual = new Date()
+    dataAtual .setHours(0)
+    dataAtual .setMinutes(0)
+    dataAtual .setSeconds(0)
+    dataAtual .setMilliseconds(0)
+
+    const anoAtual = dataAtual.getFullYear()
+
+    const dataNiver = new Date (anoAtual, m - 1, d)
+
+    const dataAtualTimeStamp = +dataAtual//ou so usa o atalho: getTime 
+    const dataNiverTimeStamp = +dataNiver//ou :  getTime
+
+    if(dataAtualTimeStamp < dataAtualTimeStamp){
+        dataNiver.setFullYear(++anoAtual)
+        dataNiverTimeStamp = +dataNiver
+    }
+
+    const UM_DIA = 24 * 60 * 60 * 1000 //conta para um dia em milesegundos
+    const diferenca = dataNiverTimeStamp - dataAtualTimeStamp
+    return parseInt(diferenca / UM_DIA)
+
 }
